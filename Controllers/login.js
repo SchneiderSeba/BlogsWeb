a028f631e6a2bd053d5515339da74bd4cf6ad39b
SchneiderSeba <seba_19_sc@hotmail.com>
2024-10-12 16:30:49 +0100

    devolver id tambn

--- Controllers/login.js
+++ Controllers/login.js
@@ -30,5 +30,5 @@ loginRouter.post('/', async (req, res) => {
 
   res
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, username: user.username, name: user.name, id: user._id })
 })
